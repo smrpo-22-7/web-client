cb0d828f5c52839581ce02ff0fafd39cc8283166
Miha Jamsek <miha_jamsek@windowslive.com>
2022-03-07 19:48:34 +0100

    added external ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { AppEnvironment } from "./env.types";
 export const environment: AppEnvironment = {
     production: true,
     service: {
-        apiUrl: "http://localhost:8080/v1",
-        authUrl: "http://localhost:8080",
+        apiUrl: "http://188.34.196.239:8080/v1",
+        authUrl: "http://188.34.196.239",
     }
 };

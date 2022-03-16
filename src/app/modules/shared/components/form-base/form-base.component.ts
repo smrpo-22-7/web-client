e3ec78d0c4039629abd65416e1dcd8207b2bbea2
Miha Jamsek <miha_jamsek@windowslive.com>
2022-03-16 18:13:37 +0100

    minor bugfix

--- src/app/modules/shared/components/form-base/form-base.component.ts
+++ src/app/modules/shared/components/form-base/form-base.component.ts
@@ -1,10 +1,6 @@
-import { Component } from "@angular/core";
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 import { isEmpty } from "lodash";
 
-@Component({
-    selector: "sc-form-base",
-})
 export abstract class FormBaseComponent {
    
     public hasError(control: AbstractControl | null, error?: keyof ValidationErrors | string): boolean {

32ee20ae750e09aa3ceeefb281a6e10a428b7178
Miha Jamsek <miha_jamsek@windowslive.com>
2022-03-20 15:07:35 +0100

    added stories module

---
+++ src/app/modules/stories/pages/project-stories-page/project-stories-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "sc-project-stories-page",
+    templateUrl: "./project-stories-page.component.html",
+    styleUrls: ["./project-stories-page.component.scss"]
+})
+export class ProjectStoriesPageComponent implements OnInit {
+    
+    constructor() {
+    }
+    
+    ngOnInit(): void {
+    }
+    
+}

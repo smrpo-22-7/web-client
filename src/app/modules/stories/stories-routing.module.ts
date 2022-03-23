2e1c8d16609e3fb2aacb0ad1fa5ec668319a5c2b
Miha Jamsek <miha_jamsek@windowslive.com>
2022-03-23 09:24:12 +0100

    fixed navigation

--- src/app/modules/stories/stories-routing.module.ts
+++ src/app/modules/stories/stories-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ProjectStoriesPageComponent } from "./pages/project-stories-page/project-stories-page.component";
 import { StoryFormPageComponent } from "./pages/story-form-page/story-form-page.component";
-import { SprintFormPageComponent } from "../sprints/pages/sprint-form-page/sprint-form-page.component";
 
 
 const routes: Routes = [
     { path: "", pathMatch: "full", component: ProjectStoriesPageComponent },
-    { path: "story/new", component: StoryFormPageComponent },
-    { path: "sprint/new", component: SprintFormPageComponent }
+    { path: "new", component: StoryFormPageComponent }
 
 ];
 

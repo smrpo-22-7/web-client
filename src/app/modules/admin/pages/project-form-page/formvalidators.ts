b220264bd2bff8d0e7e673a34a46e89bc43bd205
Miha Jamsek <miha_jamsek@windowslive.com>
2022-04-08 20:07:15 +0200

    added project role validation

--- src/app/modules/admin/pages/project-form-page/formvalidators.ts
+++ src/app/modules/admin/pages/project-form-page/formvalidators.ts
@@ -1,5 +1,6 @@
 import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors } from "@angular/forms";
 import { SysRole } from "@lib";
+import { ProjectRole } from "@config/roles.config";
 
 /*+import { FormGroup, ValidationErrors } from "@angular/forms";
 
@@ -18,3 +19,26 @@ export function doubleUsers(formGroup: FormGroup): ValidationErrors | null {
     }
     return null;
 }**/
+
+export function validateUsersAndRoles(formGroup: FormGroup): ValidationErrors | null {
+    const usersArray = formGroup.get("members") as FormArray;
+    
+    const errors: ValidationErrors = {};
+    let hasErrors = false;
+    
+    if (usersArray.length === 0) {
+        hasErrors = true;
+        errors["noMembers"] = true;
+    }
+    
+    const hasRequiredRoles =
+        usersArray.controls.filter(ctrl => ctrl.value === ProjectRole.SCRUM_MASTER).length === 1 &&
+        usersArray.controls.filter(ctrl => ctrl.value === ProjectRole.PRODUCT_OWNER).length === 1 &&
+        usersArray.controls.filter(ctrl => ctrl.value === ProjectRole.MEMBER).length >= 1;
+    if (!hasRequiredRoles) {
+        hasErrors = true;
+        errors["missingRoles"] = true;
+    }
+    
+    return hasErrors ? errors : null;
+}
--- src/app/modules/admin/pages/project-form-page/project-form-page.component.ts
+++ src/app/modules/admin/pages/project-form-page/project-form-page.component.ts
@@ -5,9 +5,11 @@ import { filter, Observable, Subject, switchMap, take, takeUntil } from "rxjs";
 
 import { ProjectRole, User, isProjectRegisterRequest } from "@lib";
 import { ProjectService, RoleService, UserService } from "@services";
-import {validateUniqueProjectName} from "@utils";
+import { validateUniqueProjectName } from "@utils";
 import { FormBaseComponent } from "@shared/components/form-base/form-base.component";
 import { Router } from "@angular/router";
+import { validateUsersAndRoles } from "./formvalidators";
+import { ProjectRole as ProjectRoleConfig } from "@config/roles.config";
 
 @Component({
     selector: "sc-project-form-page",
@@ -15,13 +17,12 @@ import { Router } from "@angular/router";
     styleUrls: ["./project-form-page.component.scss"]
 })
 export class ProjectFormPageComponent extends FormBaseComponent implements OnInit, OnDestroy {
-
+    
     public roles$: Observable<ProjectRole[]>;
     public users$: Observable<User[]>;
     public projectForm: FormGroup;
     private destroy$ = new Subject<boolean>();
-    public check: boolean;
-
+    
     constructor(private fb: FormBuilder,
                 private roleService: RoleService,
                 private projectService: ProjectService,
@@ -30,18 +31,18 @@ export class ProjectFormPageComponent extends FormBaseComponent implements OnIni
                 private toastrService: ToastrService) {
         super();
     }
-
+    
     ngOnInit(): void {
         this.projectForm = this.fb.group({
-            name: this.fb.control("",[Validators.required], [validateUniqueProjectName(this.projectService)]),
+            name: this.fb.control("", [Validators.required], [validateUniqueProjectName(this.projectService)]),
             members: this.fb.array([]),
             userQuery: this.fb.control(""),
-        });
-
+        }, { validators: validateUsersAndRoles });
+        
         this.roles$ = this.projectService.getProjectRoles().pipe(
             takeUntil(this.destroy$),
         );
-
+        
         this.users$ = this.userQueryCtrl.valueChanges.pipe(
             filter((value: string) => {
                 return value.length > 0;
@@ -51,83 +52,69 @@ export class ProjectFormPageComponent extends FormBaseComponent implements OnIni
             }),
             takeUntil(this.destroy$)
         );
-
-        this.check=true;
     }
-
-    public isDisabled(idRole: string): boolean {
-        const formValue = this.projectForm.getRawValue();
-        for (let val of formValue["members"]) {
-            if (val.projectRoleId == "product_owner" && idRole == "product_owner") {
-                return true;
-            }
-        }
-        for (let val of formValue["members"]) {
-            if (val.projectRoleId =="scrum_master" && idRole == "scrum_master") {
-                return true;
-            }
-        }
-        return false;
+    
+    public isDisabled(roleId: string): boolean {
+        return this.usersCtrl.controls.some(ctrl => {
+            return roleId !== ProjectRoleConfig.MEMBER && ctrl.get("projectRoleId")?.value === roleId;
+        });
     }
-
+    
     public onUserSelect($event: User): void {
         this.userQueryCtrl.setValue("");
-        const formValue = this.projectForm.getRawValue();
-        for (let val of formValue["members"]) {
-            if (val.userName == $event.username) {
-                this.check=false;
-                break;
-            }
+        
+        const existingUser = this.usersCtrl.controls.some(ctrl => ctrl.get("userId")?.value === $event.id);
+        if (existingUser) {
+            return;
         }
-        if (this.check) {
-            this.usersCtrl.push(this.fb.group({
-                userName: this.fb.control($event.username),
-                userId: this.fb.control($event.id),
-                projectRoleId: this.fb.control("member"),
-            }));
-        }
-        this.check=true;
+        
+        this.usersCtrl.push(this.createUserFormGroup($event.username, $event.id));
     }
-
-    removeUser(index: number): void {
+    
+    private createUserFormGroup(username: string, userId: string): FormGroup {
+        return this.fb.group({
+            userName: this.fb.control(username),
+            userId: this.fb.control(userId),
+            projectRoleId: this.fb.control("member"),
+        });
+    }
+    
+    public removeUser(index: number): void {
         this.usersCtrl.removeAt(index);
     }
-
-
+    
     public createProject() {
         const formValue = this.projectForm.getRawValue();
         delete formValue["userQuery"];
         for (let val of formValue["members"]) {
             delete val.userName;
         }
-        console.log(formValue);
         if (isProjectRegisterRequest(formValue)) {
             this.projectService.createProject(formValue).pipe(take(1)).subscribe({
                 next: () => {
-                    console.log("created!");
                     this.toastrService.success("New project was added!", "Success!");
                     this.projectForm.reset();
-                    this.projectForm.controls["members"].reset();
+                    this.usersCtrl.reset();
                     this.router.navigate(["/admin/projects"]);
                 },
                 error: err => {
-                    console.log("NAPAKA!");
                     console.error(err);
+                    this.toastrService.error("Server error when creating project!", "Error!");
                 }
             });
         } else {
             throw new TypeError("Form does not match the required format!");
         }
     }
-
+    
     ngOnDestroy() {
         this.destroy$.next(true);
     }
-
+    
     public get usersCtrl(): FormArray {
         return this.projectForm.controls["members"] as FormArray;
     }
-
+    
     public get userQueryCtrl(): FormControl {
         return this.projectForm.controls["userQuery"] as FormControl;
     }

b9a2ab6ca9cb7cf2968f8a97301744fcc6690dca
Miha Jamsek <miha_jamsek@windowslive.com>
2022-04-28 16:23:56 +0200

    bugfix

--- src/app/modules/stories/pages/edit-story/edit-story.component.ts
+++ src/app/modules/stories/pages/edit-story/edit-story.component.ts
@@ -1,41 +1,40 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBaseComponent} from "@shared/components/form-base/form-base.component";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBaseComponent } from "@shared/components/form-base/form-base.component";
 import {
     Codebooks,
     isStoryRegisterRequest,
     NavState,
     NavStateStatus,
-    Story,
     StoryRegisterRequest,
-    UserProfile
 } from "@lib";
-import {map, Observable, startWith, Subject, switchMap, take, takeUntil, tap} from "rxjs";
-import {StoryPriorityLabel} from "@config/enums.config";
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {CodebookService, StoryService} from "@services";
-import {ToastrService} from "ngx-toastr";
-import {ActivatedRoute, ParamMap, Router} from "@angular/router";
-import {NavContext} from "@context";
-import {validateUniqueStoryTitle, validateUniqueStoryTitleOne} from "../story-form-page/validators";
+import { map, Observable, startWith, Subject, switchMap, take, takeUntil, tap } from "rxjs";
+import { StoryPriorityLabel } from "@config/enums.config";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { CodebookService, StoryService } from "@services";
+import { ToastrService } from "ngx-toastr";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { NavContext } from "@context";
+import { validateUniqueStoryTitleOne } from "../story-form-page/validators";
 
 @Component({
-  selector: 'sc-edit-story',
-  templateUrl: './edit-story.component.html',
-  styleUrls: ['./edit-story.component.scss']
+    selector: "sc-edit-story",
+    templateUrl: "./edit-story.component.html",
+    styleUrls: ["./edit-story.component.scss"]
 })
 export class EditStoryComponent extends FormBaseComponent implements OnInit, OnDestroy {
-
+    
     public storyId$: Observable<string>;
-
+    public projectId$: Observable<string>;
+    
     public navStates = NavStateStatus;
     public nav$: Observable<NavState>;
     public priorities$: Observable<string[]>;
     public priorityLabels = StoryPriorityLabel;
-
+    
     public storyForm: FormGroup;
     public testInputCtrl: FormControl;
     private destroy$ = new Subject<boolean>();
-
+    
     constructor(private fb: FormBuilder,
                 private storypriorityservice: StoryService,
                 private toastrService: ToastrService,
@@ -46,7 +45,7 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
                 private storyService: StoryService) {
         super();
     }
-
+    
     ngOnInit(): void {
         this.storyForm = this.fb.group({
             title: this.fb.control("", [Validators.required]),
@@ -56,25 +55,32 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
             priority: this.fb.control("", [Validators.required]),
             businessValue: this.fb.control(1, [Validators.min(1), Validators.max(10)]),
             timeEstimate: this.fb.control(null, [Validators.min(1)]),
-        } , { asyncValidators: [validateUniqueStoryTitleOne(this.route.snapshot.params["projectId"]!, this.storypriorityservice)] } );
-
+        }, { asyncValidators: [validateUniqueStoryTitleOne(this.route.snapshot.params["projectId"]!, this.storypriorityservice)] });
+        
         this.testInputCtrl = this.fb.control("");
-
+        
         this.priorities$ = this.codebookService.getCodebook(Codebooks.StoryPriority).pipe(
             takeUntil(this.destroy$),
         );
-
+        
         this.nav$ = this.nav.getNavState().pipe(
             takeUntil(this.destroy$)
         );
-
+        
         this.storyId$ = this.route.paramMap.pipe(
             startWith(this.route.snapshot.paramMap),
             map((paramMap: ParamMap) => {
                 return paramMap.get("storyId") as string;
             }),
         );
-
+    
+        this.projectId$ = this.route.paramMap.pipe(
+            startWith(this.route.snapshot.paramMap),
+            map((paramMap: ParamMap) => {
+                return paramMap.get("projectId") as string;
+            }),
+        );
+        
         this.storyId$.pipe(
             switchMap((storyId: string) => {
                 return this.storyService.getStoryById(storyId);
@@ -84,7 +90,7 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
                     ...story,
                     oldtitle: story.title
                 }, { emitEvent: false });
-
+                
                 this.sysTestsCtrl.clear();
                 story.tests.forEach(role => {
                     this.sysTestsCtrl.push(this.fb.control(role.result));
@@ -97,7 +103,7 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
             this.storyForm.markAsPristine();
         });
     }
-
+    
     public addTest() {
         if (this.testInputCtrl.value) {
             this.sysTestsCtrl.push(this.fb.control(this.testInputCtrl.value));
@@ -106,11 +112,11 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
             this.toastrService.error("Test can't be empty!", "Error!")
         }
     }
-
+    
     public removeTest(index: number): void {
         this.sysTestsCtrl.removeAt(index);
     }
-
+    
     public editStory(projectId: string) {
         const formValue = this.storyForm.getRawValue();
         formValue["tests"] = formValue["tests"].map((x: string) => ({ result: x }));
@@ -135,13 +141,19 @@ export class EditStoryComponent extends FormBaseComponent implements OnInit, OnD
             throw new TypeError("Form does not match the required format!");
         }
     }
-
+    
+    public goBack() {
+        this.projectId$.pipe(take(1)).subscribe(projectId => {
+            this.router.navigate(["projects", projectId, "stories"]);
+        });
+    }
+    
     ngOnDestroy() {
         this.destroy$.next(true);
     }
-
+    
     public get sysTestsCtrl(): FormArray {
         return this.storyForm.controls["tests"] as FormArray;
     }
-
+    
 }

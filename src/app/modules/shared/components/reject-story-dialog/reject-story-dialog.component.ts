cff3c30fe83b373fb913a9a15dfd60ed522cef4d
Miha Jamsek <miha_jamsek@windowslive.com>
2022-05-01 17:12:45 +0200

    added reject reason as md editor

--- src/app/modules/shared/components/reject-story-dialog/reject-story-dialog.component.ts
+++ src/app/modules/shared/components/reject-story-dialog/reject-story-dialog.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { StoryService } from "@services";
 import { BsModalRef } from "ngx-bootstrap/modal";
 import { take } from "rxjs";
 import { ToastrService } from "ngx-toastr";
 import { VoidFunc } from "@mjamsek/prog-utils";
 import { ExtendedStory } from "@lib";
+import { FormBaseComponent } from "@shared/components/form-base/form-base.component";
+import SimpleMDE from "simplemde";
 
 @Component({
     selector: "sc-reject-story-dialog",
     templateUrl: "./reject-story-dialog.component.html",
     styleUrls: ["./reject-story-dialog.component.scss"]
 })
-export class RejectStoryDialogComponent implements OnInit {
+export class RejectStoryDialogComponent extends FormBaseComponent implements OnInit, AfterViewInit, OnDestroy {
     
     public story: ExtendedStory;
     public onReject: VoidFunc = () => {};
     
     public rejectForm: FormGroup;
+    private editorRef: ElementRef<HTMLDivElement>;
+    private editorInstance: SimpleMDE;
     
     constructor(private fb: FormBuilder,
                 private ref: BsModalRef,
                 private toastrService: ToastrService,
                 private storyService: StoryService) {
+        super();
     }
     
     ngOnInit(): void {
         this.rejectForm = this.fb.group({
-            rejectComment: this.fb.control(""),
+            rejectComment: this.fb.control("", [Validators.required]),
+        });
+    }
+    
+    ngAfterViewInit() {
+        this.editorInstance = new SimpleMDE({ element: this.editorRef.nativeElement });
+        this.editorInstance.codemirror.on("change", () => {
+            this.commentCtrl.setValue(this.editorInstance.value(), { emitEvent: false });
         });
     }
     
@@ -53,7 +65,17 @@ export class RejectStoryDialogComponent implements OnInit {
         });
     }
     
+    ngOnDestroy() {
+    }
+    
     public get commentCtrl(): FormControl {
         return this.rejectForm.get("rejectComment") as FormControl;
+    }
+    
+    @ViewChild("mdEditor", { static: false })
+    public set editorReference(content: ElementRef<HTMLDivElement>) {
+        if (content) {
+            this.editorRef = content;
+        }
     }
 }
--- src/app/modules/shared/components/story-list-content/story-list-content.component.ts
+++ src/app/modules/shared/components/story-list-content/story-list-content.component.ts
@@ -120,6 +120,7 @@ export class StoryListContentComponent implements OnInit {
                     this.whenUpdated.emit();
                 },
             },
+            class: "modal-lg",
         });
     }
     
